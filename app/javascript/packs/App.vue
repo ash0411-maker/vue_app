9b3cfb44d1a3d8f66fdab9c45e70599178a2f11f
ash <uetani.0411@gmail.com>
2020-09-19 18:28:13 +0900

    [add]route.jsの追加

--- app/javascript/packs/App.vue
+++ app/javascript/packs/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <!-- router.jsで登録されたコンポーネントが表示される場所 -->
+            <router-view/>
+    </div>
 </template>
 
 <script>
     export default {
+        // VueDevtoolsにおけるコンポーネントの識別名称
         name: 'App',
     }
 </script>
