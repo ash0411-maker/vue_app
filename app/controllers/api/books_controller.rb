5e9c68259e5f15133c92f65b3c2d235742cc6fd4
ash <uetani.0411@gmail.com>
2020-09-19 18:22:54 +0900

    [modified] APIの作成

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,6 +1,6 @@
 class Api::BooksController < ApplicationController
-  def show
-    @book = Book.find(params[:id])
-    render "show", formats: :json, handlers: "jbuilder"
-  end
+    def index
+        @books = Book.all
+        render 'index', formats: :json, handlers: 'jbuilder'
+    end
 end
--- app/views/api/books/show.json.jbuilder
+++
@@ -1,5 +0,0 @@
-json.id        @book.id
-json.title     @book.title
-json.author    @book.author
-json.publisher @book.publisher
-json.genre     @book.genre
