2d62c0bfcb98221823ac6779b2feb58aae12eafd
ash <uetani.0411@gmail.com>
2020-09-19 15:11:10 +0900

    [add] Booksテーブルの作成

---
+++ app/javascript/packs/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return: {
+            message: "初めてrails&Vue.js"
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    p {
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
